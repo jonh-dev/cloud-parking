b886e42b1cd0b5f60bf449eef83662143d04fa6d
jonh-dev <jonh.dev.br@gmail.com>
2022-08-02 17:21:48 -0300

    First Commit, start project

---
+++ src/main/java/one/digitalinnovation/cloudparking/controller/HelloController.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.cloudparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping
+    public String Hello(){
+        return "Hello DIO.. Bootcamp Spring Boot.";
+    }
+}
